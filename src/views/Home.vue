d9316f9065e01fa2eb3e64ffc969d8314efc3b2e
technodromex <ricardo.guzmantorres@owasp.org>
2021-04-27 14:29:17 -0500

    Add modal component

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,12 +1,29 @@
 <template>
   <h1 class="main-title">Welcome!</h1>
+  <button id="show-modal" @click="showModal = true">Show Modal</button>
+  <!-- use the modal component, pass in the prop -->
+  <modal v-if="showModal" @close="showModal = false">
+    <template v-slot:header>
+      <h3>custom header</h3>
+    </template>
+  </modal>
 </template>
 
 <script lang="ts">
-import { defineComponent } from 'vue';
+import { defineComponent, ref } from 'vue';
+import Modal from '../components/Modal.vue';
 
 export default defineComponent({
   // type inference enabled
+  components: {
+  	'modal': Modal
+  },
+	setup() {
+		const showModal = ref(false);
+		return {
+			showModal
+		}
+  }
 })
 
 </script>
